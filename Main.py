e5814c3656b5fa18b6c595a886b7a641f2d4a4a9
FGZone <65375660+FGZone1@users.noreply.github.com>
2023-06-03 00:10:13 -0300

    Conexion Base probando

--- Main.py
+++ Main.py
@@ -1,1 +1,10 @@
-pepe
+import mysql.connector
+
+cnx = mysql.connector.connect(user='root', password='FernetFree2023',
+                              host='127.0.0.1',
+                              database='sensores',
+                              use_pure=False)
+cursor = cnx.cursor()
+query = ("SELECT * FROM pepe ")
+cursor.close()
+cnx.close()
