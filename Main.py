c9775b166e80b4ca0d63483a45b694acc13b18f1
bautibustos <bautibustos10@gmail.com>
2023-06-10 01:44:00 -0300

    ultima version con alerta en bd

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import ConsultaBD as bd
+import Alerta as A
 
 def main():
     datitos=bd.Consulta()
@@ -7,6 +8,7 @@ def main():
     for i in datitos:#  i es el indice 
         print(datitos[i]['temperatura'])
         if i>1:
-          if datitos[i-1]['temperatura']-datitos[i]['temperatura']>=3:
-            print('aaaaa')
+            if datitos[i-1]['temperatura']-datitos[i]['temperatura']>=3:
+                print('juro por dios que ando')
+                A.aviso(datitos,i)
 main() 
