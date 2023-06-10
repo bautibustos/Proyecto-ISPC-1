720ace3713150d4383d049242d5d596b8db97b00
bautibustos <bautibustos10@gmail.com>
2023-06-09 21:06:38 -0300

    cambio test

--- Main.py
+++ Main.py
@@ -7,18 +7,14 @@ cnx = mysql.connector.connect(user='root', password='FernetFree2023',
 cursor = cnx.cursor()
 query = ("SELECT * FROM registro")
 cursor.execute(query)
+
 datos={}
-
-
-for (id_registro,Fecha,Temperatura,Humedad,Presion) in cursor:
-  datos[f'{id_registro}']=[]
-  datos[''].append({
-					#"id":(f"ID {idgame[cont]}"),
-					"title": titulo[i],
-					"platform": plat[i],
-					"photo": imagen[i],
-					"link": f"{link[i]} ",
-					"description":desc[i]
+for (id_registro,Fecha,Temperatura,Humedad,Presion) in cursor: 
+  datos[id_registro]=[]
+  datos[id_registro].append({
+					"temperatura": Temperatura,
+					"presion": Presion,
+					"humedad": Humedad,
 					})
   diccionario={id_registro,Fecha,Temperatura,Humedad,Presion}
   print(diccionario)
