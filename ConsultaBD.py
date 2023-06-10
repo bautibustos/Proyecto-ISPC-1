eae0bf83ecf410817c4ac0d6cefcc8447a3b2df1
bautibustos <bautibustos10@gmail.com>
2023-06-09 21:41:59 -0300

    cambios de archivos

---
+++ ConsultaBD.py
@@ -0,0 +1,28 @@
+def Consulta():
+  import mysql.connector
+
+  cnx = mysql.connector.connect(user='root', password='FernetFree2023',
+                                host='127.0.0.1',
+                                database='sensores',
+                                use_pure=False)
+  cursor = cnx.cursor()
+  query = ("SELECT * FROM registro")
+  cursor.execute(query)
+
+  datos={}
+
+  for (id_registro,Fecha,Temperatura,Humedad,Presion) in cursor: 
+    datos[f"{id_registro}"]={
+            "fecha": Fecha,
+            "temperatura": Temperatura,
+            "presion": Presion,
+            "humedad": Humedad,
+            }
+
+  #print(datos)
+  #print (datos["1"]["fecha"][8:10]) #Hora
+  #print (datos["1"]["fecha"][:8]) #Fecha
+
+  cursor.close()
+  cnx.close()
+  return datos
