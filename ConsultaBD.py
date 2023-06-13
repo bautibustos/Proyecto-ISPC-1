a40a5e42c84775880c1a8896a3d68a87e200ff29
FGZone <65375660+FGZone1@users.noreply.github.com>
2023-06-13 19:35:16 -0300

    ..p

--- ConsultaBD.py
+++ ConsultaBD.py
@@ -33,4 +33,4 @@ def Consulta():
   cnx.close()
   return datos
 
-Consulta()
+#Consulta()
--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import ConsultaBD as bd
 import Alerta as A
+from datetime import datetime
 
 def main():
     bd.Consulta()
@@ -7,8 +8,9 @@ def main():
                     'Ingrese 2 si quiere ingresar las fechas\n'
                     'Ingrese 3 si quiere ver el promedio de la Humedad \n'
                    ))
+    datitos=bd.Consulta()# traigo la lista de datos.
     if Menu==1:#este es el menu de la alerta
-        datitos=bd.Consulta()# traigo la lista de datos.
+       
         for i in datitos:#  i es el indice 
             print(datitos[i]['temperatura'])#print de checkeos
             if i>1:
@@ -20,7 +22,19 @@ def main():
                     print('juro por dios que ando')
                     A.aviso(datitos,i)
     elif Menu == 2:# este es la seleccion de la fecha de inicio y de final
-        print('si')
+        print (f"En la base simulamos datos desde el {datitos[1]['fecha']} hasta el {datitos[len(datitos)]['fecha']}")
+        while True:
+            try:
+                fechainicio=str(input('\nIngrese Fecha Inicio con el siguiente formato (DDMMYY)\n'))
+                fechafin=str(input('\nIngrese Fecha Fin con el siguiente formato (DDMMYY)\n'))
+                datetime.strptime(fechainicio, '%d%m%y')
+                datetime.strptime(fechafin, '%d%m%y')
+                print("Fecha válida")
+                break
+            except ValueError:
+                print('Ingrese la fecha con el formato que corresponde')
+        
+
     elif Menu == 3: #aca usamos el promedio
         archivo=open("PromHumedad.txt","r")
         print('El promedio de la Humedad es: ',archivo.read()) #Leemos y mostramos el contenido del archivo que guardamos en consulta BD
